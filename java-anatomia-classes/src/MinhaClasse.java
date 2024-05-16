9eb851ac3f7e4665de4f88fba67208589dc7ada2
manuzel <emanuelvictor.dev@gmail.com>
2024-05-16 15:25:58 -0300

    Aula anatomia de classes 2

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+    public static void main (String [] args) {
+        System.out.print ("Olá Mundo bem vindos ao ano de:");
+        int ano = 2024;
+        System.out.print (ano);
+
+
+    }
+}
