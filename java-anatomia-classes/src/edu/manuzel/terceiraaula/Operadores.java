67db4b9c06883747fe6eebfb2465bda74b10a2f5
manuzel <emanuelvictor.dev@gmail.com>
2024-05-21 16:40:33 -0300

    Operadores

--- java-anatomia-classes/src/edu/manuzel/terceiraaula/Operadores.java
+++ java-anatomia-classes/src/edu/manuzel/terceiraaula/Operadores.java
@@ -2,7 +2,63 @@ package edu.manuzel.terceiraaula;
     public class Operadores{
 
         public static void main(String[] args) throws Exception {
-            int numero = 5;
-            System.out.println( - numero);
+           boolean condicao1 = true;
+           boolean condicao2 = false;
+           if( condicao1 && condicao2){
+                System.out.println("Nossas condições são verdadeiras");
+           }
+           if (condicao1 || condicao2){
+                System.out.println("Uma das condições é verdadeira");
+           }
+           
+           /*
+            String nome1 = "Cleyson";
+           String nome2 = new String("Cleyson");
+
+           System.out.println(nome1.equals(nome2));
+            
+            int a, b;
+            a = 5;
+            b = 6;
+            
+            String resultado = " ";
+
+            if (a==b)
+                resultado = "True";
+            else
+                resultado = "Falso";
+
+            //Operador Ternário
+            String resultado = a==b ? "Verdadeiro" : "falso";
+
+
+            System.out.println(resultado);
+            
+
+            int numero1 = 1;
+            int numero2 = 2;
+
+            boolean simNao = numero1 == numero2;
+
+            if (numero1 < numero2){
+                System.out.println("Nossa condição é verdadeira");
+            }
+            
+            System.out.println("NumeroUm é igual a numeroDois? " + simNao);
+
+            simNao = numero1 != numero2;
+
+            System.out.println("NumeroUm é diferente de numeroDois? " + simNao);
+
+            simNao = numero1 > numero2;
+
+            System.out.println("NumeroUm é maior a numeroDois? " + simNao);
+
+            simNao = numero1 < numero2;
+
+            System.out.println("NumeroUm é menor a numeroDois? " + simNao);
+*/
+
+
         }
     }
