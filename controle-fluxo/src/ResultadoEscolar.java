a39eb4bed671069e303e441b0ea81f7b2f743e16
manuzel <emanuelvictor.dev@gmail.com>
2024-05-29 15:19:44 -0300

    algumas alterações basicas

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,22 @@
 public class ResultadoEscolar{
 
     public static void main (String [] args) {
-        int nota = 8;
+        int nota2 =5;
+
+        String resultado = nota2 >=7 ? "Aprovado" : nota2 >=5 && nota2<7 ? "Exame" : "Reprovado";
+
+        System.out.println(resultado);
+        
+        /*int nota = 5;
 
         if (nota >=7 )
             System.out.println("Aprovado");
+
+            else if (nota>=5 && nota <7)
+            System.out.println("Exame");
+
         else
-            System.out.println("Reprovado");
+            System.out.println("Reprovado");*/
 
     }
 
